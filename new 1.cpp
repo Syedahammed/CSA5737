0a342c083944ef979d3af1ce275a926a0a4e6005
Syedahammed <122341900+Syedahammed@users.noreply.github.com>
2023-01-10 13:20:44 +0530

    Add files via upload

---
+++ new 1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i=0,n;
+	printf("enter no. of terms: ");
+	scanf("%d",&n);
+	{
+	while(i<=n)
+	{
+	
+	printf("%d \n",i);
+	i++; 
+	}
+			
+			
+			
+		}
+		
+	}
+
